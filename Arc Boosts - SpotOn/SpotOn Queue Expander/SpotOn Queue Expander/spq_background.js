dba725bb974c3e53a8ce37fd5dd4ed4b9da6761d
Kami on YT <103985728+SenpaiHunters@users.noreply.github.com>
2023-06-11 19:34:09 +1000

    Update spq_background.js
    Uploaded the wrong file, here is the new one
    
    Signed-off-by: Kami on YT <103985728+SenpaiHunters@users.noreply.github.com>

--- Arc Boosts - SpotOn/SpotOn Queue Expander/SpotOn Queue Expander/spq_background.js
+++ Arc Boosts - SpotOn/SpotOn Queue Expander/SpotOn Queue Expander/spq_background.js
@@ -1,26 +1,37 @@
 function bytesToJson(bytes) {
-	const intArr = new Uint8Array(bytes);
-	var unparsedString = "";
-	
-	for (const i of intArr) {
-		unparsedString += String.fromCharCode(i);
-	}
-	
-	const parsedJson = JSON.parse(unparsedString);
-	return parsedJson;
+  const byteArray = new Uint8Array(bytes);
+  let jsonString = "";
+  
+  for (const byte of byteArray) {
+    jsonString += String.fromCharCode(byte);
+  }
+  
+  let parsedJson;
+  try {
+    parsedJson = JSON.parse(jsonString);
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    return null;
+  }
+  
+  return parsedJson;
 }
 
-// Used to obtain device and connection IDs
+function sendDeviceAndConnectionIds(deviceId, connectionId) {
+  chrome.tabs.query({ url: "https://open.spotify.com/*" }, function(tabs) {
+    for (const tab of tabs) {
+      chrome.tabs.sendMessage(tab.id, { deviceId: deviceId, connectionId: connectionId });
+    }
+  });
+}
+
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-	if (details.url.endsWith("spclient.spotify.com/track-playback/v1/devices")) {
-		const data = bytesToJson(details.requestBody.raw[0].bytes);
-		const deviceId = data.device.device_id;
-		const connectionId = data.connection_id;
-		
-		chrome.tabs.query({url: "https://open.spotify.com/*"}, function(tabs) {
-			for (const tab of tabs) {
-				chrome.tabs.sendMessage(tab.id, {deviceId: deviceId, connectionId: connectionId});
-			}
-		});
-	}
+  if (details.url.endsWith("spclient.spotify.com/track-playback/v1/devices")) {
+    const data = bytesToJson(details.requestBody.raw[0].bytes);
+    if (data) {
+      const deviceId = data.device.device_id;
+      const connectionId = data.connection_id;
+      sendDeviceAndConnectionIds(deviceId, connectionId);
+    }
+  }
 }, { urls: ["<all_urls>"] }, ["requestBody"]);
